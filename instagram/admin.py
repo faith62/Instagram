81e63e3985c31df407ef7b3bcde46def66d74e1b
faith62 <phifeichemu@gmail.com>
2022-06-05 14:44:23 +0300

    add models to the admin

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Profile,Image
+
+admin.site.register(Image)
+admin.site.register(Profile)
