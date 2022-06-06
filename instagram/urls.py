ddb7075bd1eefb4de8473248ddf65b2e0e2c7857
faith62 <phifeichemu@gmail.com>
2022-06-06 10:06:28 +0300

    add media url

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,11 @@
 from django.urls import re_path,path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.index, name='indexPage'),
     path('new/image', views.new_image, name='new-image')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
